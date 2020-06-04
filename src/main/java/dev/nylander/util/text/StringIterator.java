2019f4b96e3ab82620db2cd09aaac434fee8d57f
nylanderdev <rasmus.s.nylander@gmail.com>
2020-06-04 20:04:19 +0200

    Init commit

---
+++ src/main/java/dev/nylander/util/text/StringIterator.java
@@ -0,0 +1,24 @@
+package dev.nylander.util.text;
+
+import java.util.Iterator;
+
+public class StringIterator implements Iterator<Character> {
+    private final Character[] charactersOfString;
+    private int nextIndex = 0;
+
+    public StringIterator(String string) {
+        char[] primitiveArray = string.toCharArray();
+        charactersOfString = new Character[primitiveArray.length];
+        for (int i = 0; i < primitiveArray.length; i++) {
+            charactersOfString[i] = primitiveArray[i];
+        }
+    }
+
+    public boolean hasNext() {
+        return nextIndex < charactersOfString.length;
+    }
+
+    public Character next() {
+        return charactersOfString[nextIndex++];
+    }
+}
